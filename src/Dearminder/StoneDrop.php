bf8285b15180ca9f3342f79cc5cc1b77eb02364f
Dearminder <androidwiktor@gmail.com>
2019-05-12 13:29:06 +0200

    Diamonds drop fixed (propably)

--- src/Dearminder/StoneDrop.php
+++ src/Dearminder/StoneDrop.php
@@ -21,15 +21,15 @@ class StoneDrop extends PluginBase implements Listener{
 			$ItemInHand = $player->getInventory()->getItemInHand()->getId();
 			$block = $ev->getBlock();
 			$depth = $block->GetY();
-			$random_stuff = mt_rand(1, 100);
-			$xp_coal = mt_rand(0, 3);
-			$xp_redstone = mt_rand(1, 5);
-			$xp_diamemer = mt_rand(3, 7);
-			$xp_lapis = mt_rand(2, 5);
+			$random_stuff = mt_rand(1, 10000);
+			$xp_coal = mt_rand(0, 3);/**random amount of xp for coal*/
+			$xp_redstone = mt_rand(1, 5);/**random amount of xp for redstone*/
+			$xp_diamemer = mt_rand(3, 7);/**random amount of xp for diamond and emerald*/
+			$xp_lapis = mt_rand(2, 5);/**random amount of xp for lapis*/
 			if ($player->getGamemode() == 0){
 			if ($ItemInHand == 270 /**wooden pickaxe*/){
 				if ($block->getId() == 1){
-					if ($random_stuff >= 15 && $random_stuff < 50/**chance 20% for coal*/){
+					if ($random_stuff >= 1 && $random_stuff < 200/**chance 2% for coal*/){
 						$ev->setDrops([Item::get(263, 0, 1)]);
 						if ($xp_coal = 1){$player->addXp(1);}
 						elseif ($xp_coal = 2){$player->addXp(2);}
@@ -38,41 +38,41 @@ class StoneDrop extends PluginBase implements Listener{
 					}}
 			if ($ItemInHand == 274 /**stone pickaxe*/){
 				if ($block->getId() == 1){
-					if ($random_stuff >= 15 && $random_stuff < 50/**chance 20% for coal*/){
+					if ($random_stuff >= 1 && $random_stuff < 200/**chance 2% for coal*/){
 						$ev->setDrops([Item::get(263, 0, 1)]);
 						if ($xp_coal = 1){$player->addXp(1);}
 						elseif ($xp_coal = 2){$player->addXp(2);}
 						elseif ($xp_coal = 3) {$player->addXP(3);}
 						}
-				elseif ($random_stuff >= 50 && $random_stuff < 75)/**chance 25% for iron*/{
+				elseif ($random_stuff >= 200 && $random_stuff < 380)/**chance 1,8% for iron*/{
 						$ev->setDrops([Item::get(15, 0, 1)]);}
-				elseif ($random_stuff >= 90 && $random_stuff < 95)/**chance 5% for lapiz*/{
+				elseif ($random_stuff >= 380 && $random_stuff < 560)/**chance 1,5% for lapiz*/{
 						$ev->setDrops([Item::get(351, 4, 1)]);}
 					}}
 			if ($ItemInHand == 257 /**iron pickaxe*/){
 				if ($block->getId() == 1){
 					if ($depth <= 12)/**level of diamonds**/ {
-						if ($random_stuff >= 15 && $random_stuff < 50/**chance 20% for coal*/){
+						if ($random_stuff >= 1 && $random_stuff < 200/**chance 2% for coal*/){
 						$ev->setDrops([Item::get(263, 0, 1)]);
 						if ($xp_coal = 1){$player->addXp(1);}
 						elseif ($xp_coal = 2){$player->addXp(2);}
 						elseif ($xp_coal = 3) {$player->addXP(3);}}
-				elseif ($random_stuff >= 50 && $random_stuff < 75)/**chance 25% for iron*/{
+				elseif ($random_stuff >= 200 && $random_stuff < 380)/**chance 1,8% for iron*/{
 						$ev->setDrops([Item::get(15, 0, 1)]);}
-				elseif ($random_stuff >= 90 && $random_stuff < 95)/**chance 5% for lapiz*/{
+				elseif ($random_stuff >= 380 && $random_stuff < 560)/**chance 1,5% for lapiz*/{
 						$ev->setDrops([Item::get(351, 4, 1)]);}
-				elseif ($random_stuff >= 80 && $random_stuff < 90)/**chance 10% for gold*/{
+				elseif ($random_stuff >= 80 && $random_stuff < 90)/**chance 0,8% for gold*/{
 						$ev->setDrops([Item::get(14, 0, 1)]);}
-				elseif ($random_stuff >= 75 && $random_stuff < 80)/**chance 5% for redstone*/{
+				elseif ($random_stuff >= 560 && $random_stuff < 695)/**chance 1,35% for redstone*/{
 						$ev->setDrops([Item::get(331, 0, 1)]);}
-				elseif ($random_stuff >= 95 && $random_stuff < 100)/**chance 5% for diamond*/{
+				elseif ($random_stuff >= 695 && $random_stuff < 815)/**chance 1,2% for diamond*/{
 						$ev->setDrops([Item::get(264, 0, 1)]);
 						if ($xp_diamemer = 3){$player->addXp(3);}
 						elseif ($xp_diamemer = 4){$player->addXp(4);}
 						elseif ($xp_diamemer = 5) {$player->addXP(5);}
 						elseif ($xp_diamemer = 6) {$player->addXP(6);}
 						elseif ($xp_diamemer = 7) {$player->addXP(7);}}
-				elseif ($random_stuff <= 1)/**chance 10% for emerald*/{
+				elseif ($random_stuff >= 895 && $random_stuff < 970)/**chance 0,75% for emerald*/{
 						$ev->setDrops([Item::get(388, 0, 1)]);
 						if ($xp_diamemer = 3){$player->addXp(3);}
 						elseif ($xp_diamemer = 4){$player->addXp(4);}
@@ -81,21 +81,21 @@ class StoneDrop extends PluginBase implements Listener{
 						elseif ($xp_diamemer = 7) {$player->addXP(7);}}
 						/**End of Level*/}
 						elseif($depth > 12){
-						if ($random_stuff >= 15 && $random_stuff < 50/**chance 20% for coal*/){
+						if ($random_stuff >= 1 && $random_stuff < 200/**chance 2% for coal*/){
 						$ev->setDrops([Item::get(263, 0, 1)]);
 						if ($xp_coal = 1){$player->addXp(1);}
 						elseif ($xp_coal = 2){$player->addXp(2);}
 						elseif ($xp_coal = 3) {$player->addXP(3);}
 						}
-				elseif ($random_stuff >= 50 && $random_stuff < 75)/**chance 25% for iron*/{
+				elseif ($random_stuff >= 200 && $random_stuff < 380)/**chance 1,8% for iron*/{
 						$ev->setDrops([Item::get(15, 0, 1)]);}
-				elseif ($random_stuff >= 90 && $random_stuff < 95)/**chance 5% for lapiz*/{
+				elseif ($random_stuff >= 380 && $random_stuff < 560)/**chance 1,5% for lapiz*/{
 						$ev->setDrops([Item::get(351, 4, 1)]);}
-				elseif ($random_stuff >= 80 && $random_stuff = 90)/**chance 10% for gold*/{
+				elseif ($random_stuff >= 815 && $random_stuff < 895)/**chance 0,8% for gold*/{
 						$ev->setDrops([Item::get(14, 0, 1)]);}
-				elseif ($random_stuff >= 75 && $random_stuff < 80)/**chance 5% for redstone*/{
+				elseif ($random_stuff >= 560 && $random_stuff < 695)/**chance 1,35% for redstone*/{
 						$ev->setDrops([Item::get(331, 0, 1)]);}
-				elseif ($random_stuff <= 1)/**chance 10% for emerald*/{
+				elseif ($random_stuff >= 895 && $random_stuff < 970)/**chance 0,75% for emerald*/{
 						$ev->setDrops([Item::get(388, 0, 1)]);
 						if ($xp_diamemer = 3){$player->addXp(3);}
 						elseif ($xp_diamemer = 4){$player->addXp(4);}
@@ -105,40 +105,66 @@ class StoneDrop extends PluginBase implements Listener{
 							}	}}
 			if ($ItemInHand == 285 /**golden pickaxe*/){
 				if ($block->getId() == 1){
-					if ($random_stuff >= 15 && $random_stuff < 50/**chance 20% for coal*/){
+					if ($random_stuff >= 1 && $random_stuff < 200/**chance 2% for coal*/){
 						$ev->setDrops([Item::get(263, 0, 1)]);
 						$player->addXp(1);}
 						}
 						}
 			if ($ItemInHand == 278 /**diamond pickaxe*/){
 				if ($block->getId() == 1){
-					if ($random_stuff >= 15 && $random_stuff < 50/**chance 20% for coal*/){
+					if ($depth <= 12)/**level of diamonds**/ {
+					if ($random_stuff >= 1 && $random_stuff < 200/**chance 2% for coal*/){
 						$ev->setDrops([Item::get(263, 0, 1)]);
 						if ($xp_coal = 1){$player->addXp(1);}
 						elseif ($xp_coal = 2){$player->addXp(2);}
 						elseif ($xp_coal = 3) {$player->addXP(3);}
 						}
-				elseif ($random_stuff >= 50 && $random_stuff < 75)/**chance 25% for iron*/{
+				elseif ($random_stuff >= 200 && $random_stuff < 380)/**chance 1,8% for iron*/{
 						$ev->setDrops([Item::get(15, 0, 1)]);}
-				elseif ($random_stuff >= 90 && $random_stuff < 95)/**chance 5% for lapiz*/{
+				elseif ($random_stuff >= 380 && $random_stuff < 560)/**chance 1,5% for lapiz*/{
 						$ev->setDrops([Item::get(351, 4, 1)]);}
-				elseif ($random_stuff >= 80 && $random_stuff = 90)/**chance 10% for gold*/{
+				elseif ($random_stuff >= 815 && $random_stuff < 895)/**chance 0,8% for gold*/{
 						$ev->setDrops([Item::get(14, 0, 1)]);}
-				elseif ($random_stuff >= 75 && $random_stuff < 80)/**chance 5% for redstone*/{
+				elseif ($random_stuff >= 560 && $random_stuff < 695)/**chance 1,35% for redstone*/{
 						$ev->setDrops([Item::get(331, 0, 1)]);}
-				elseif ($random_stuff >= 95 && $random_stuff < 100)/**chance 5% for diamond*/{
+				elseif ($random_stuff >= 695 && $random_stuff < 815)/**chance 1,2% for diamond*/{
 						$ev->setDrops([Item::get(264, 0, 1)]);
 						if ($xp_diamemer = 3){$player->addXp(3);}
 						elseif ($xp_diamemer = 4){$player->addXp(4);}
 						elseif ($xp_diamemer = 5) {$player->addXP(5);}
 						elseif ($xp_diamemer = 6) {$player->addXP(6);}
 						elseif ($xp_diamemer = 7) {$player->addXP(7);}}
-				elseif ($random_stuff <= 1)/**chance 10% for emerald*/{
+				elseif ($random_stuff >= 895 && $random_stuff < 970)/**chance 0,75% for emerald*/{
 						$ev->setDrops([Item::get(388, 0, 1)]);
 						if ($xp_diamemer = 3){$player->addXp(3);}
 						elseif ($xp_diamemer = 4){$player->addXp(4);}
 						elseif ($xp_diamemer = 5) {$player->addXP(5);}
 						elseif ($xp_diamemer = 6) {$player->addXP(6);}
 						elseif ($xp_diamemer = 7) {$player->addXP(7);}}
+						}
+						/**End Of level*/
+						elseif($depth > 12){
+						if ($random_stuff >= 1 && $random_stuff < 200/**chance 2% for coal*/){
+						$ev->setDrops([Item::get(263, 0, 1)]);
+						if ($xp_coal = 1){$player->addXp(1);}
+						elseif ($xp_coal = 2){$player->addXp(2);}
+						elseif ($xp_coal = 3) {$player->addXP(3);}
+						}
+				elseif ($random_stuff >= 200 && $random_stuff < 380)/**chance 1,8% for iron*/{
+						$ev->setDrops([Item::get(15, 0, 1)]);}
+				elseif ($random_stuff >= 380 && $random_stuff < 560)/**chance 1,5% for lapiz*/{
+						$ev->setDrops([Item::get(351, 4, 1)]);}
+				elseif ($random_stuff >= 815 && $random_stuff < 895)/**chance 0,8% for gold*/{
+						$ev->setDrops([Item::get(14, 0, 1)]);}
+				elseif ($random_stuff >= 560 && $random_stuff < 695)/**chance 1,35% for redstone*/{
+						$ev->setDrops([Item::get(331, 0, 1)]);}
+				elseif ($random_stuff >= 895 && $random_stuff < 970)/**chance 0,75% for emerald*/{
+						$ev->setDrops([Item::get(388, 0, 1)]);
+						if ($xp_diamemer = 3){$player->addXp(3);}
+						elseif ($xp_diamemer = 4){$player->addXp(4);}
+						elseif ($xp_diamemer = 5) {$player->addXP(5);}
+						elseif ($xp_diamemer = 6) {$player->addXP(6);}
+						elseif ($xp_diamemer = 7) {$player->addXP(7);}}
+						}
 						}}
 						}}}
