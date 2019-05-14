da7af7a6c07b0462e9d60d6d6cc4c133e79f17b5
Dearminder <androidwiktor@gmail.com>
2019-05-14 18:42:41 +0200

    Added Permission for using command drop Added /drop - Shows all Ores drop percentage

--- src/Dearminder/StoneDrop.php
+++ src/Dearminder/StoneDrop.php
@@ -4,6 +4,8 @@ namespace Dearminder;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
 use pocketmine\utils\TextFormat;
 use pocketmine\utils\MainLogger;
 use pocketmine\entity\Effect;
@@ -31,15 +33,13 @@ class StoneDrop extends PluginBase implements Listener{
 			$xp_diamemer = mt_rand(3, 7);/**random amount of xp for diamond and emerald*/
 			$xp_lapis = mt_rand(2, 5);/**random amount of xp for lapis*/
 			if ($player->getGamemode() == 0){
+			if ($block->getId() == 1){
 			if ($ItemInHand == 270 /**wooden pickaxe*/){
-				if ($block->getId() == 1){
 					if ($percentage >= 1 && $percentage < 200/**chance 2% for coal*/){
 						$ev->setDrops([Item::get(263, 0, 1+$fortune)]);
 						$player->addXp($xp_coal);}
 						}
-					}
 			if ($ItemInHand == 274 /**stone pickaxe*/){
-				if ($block->getId() == 1){
 					if ($percentage >= 1 && $percentage < 200/**chance 2% for coal*/){
 						$ev->setDrops([Item::get(263, 0, 1+$fortune)]);
 						$player->addXp($xp_coal);}
@@ -48,9 +48,8 @@ class StoneDrop extends PluginBase implements Listener{
 				elseif ($percentage >= 380 && $percentage < 560)/**chance 1,5% for lapiz*/{
 						$ev->setDrops([Item::get(351, 4, $r_lapiz)]);
 						$player->addXp($xp_lapis);}
-					}}
+					}
 			if ($ItemInHand == 257 /**iron pickaxe*/){
-				if ($block->getId() == 1){
 					if ($depth <= 12)/**level of diamonds**/ {
 						if ($percentage >= 1 && $percentage < 200/**chance 2% for coal*/){
 						$ev->setDrops([Item::get(263, 0, 1+$fortune)]);
@@ -91,16 +90,13 @@ class StoneDrop extends PluginBase implements Listener{
 				elseif ($percentage >= 895 && $percentage < 970)/**chance 0,75% for emerald*/{
 						$ev->setDrops([Item::get(388, 0, 1+$fortune)]);
 						$player->addXp($xp_diamemer);}
-							}}}
+							}}
 			if ($ItemInHand == 285 /**golden pickaxe*/){
-				if ($block->getId() == 1){
 					if ($percentage >= 1 && $percentage < 200/**chance 2% for coal*/){
 						$ev->setDrops([Item::get(263, 0, 1+$fortune)]);
 						$player->addXp($xp_coal);}
 						}
-						}
 			if ($ItemInHand == 278 /**diamond pickaxe*/){
-				if ($block->getId() == 1){
 					if ($depth <= 12)/**level of diamonds**/ {
 					if ($percentage >= 1 && $percentage < 200/**chance 2% for coal*/){
 						$ev->setDrops([Item::get(263, 0, 1+$fortune)]);
@@ -141,9 +137,31 @@ class StoneDrop extends PluginBase implements Listener{
 						$ev->setDrops([Item::get(388, 0, 1+$fortune)]);
 						$player->addXp($xp_diamemer);}
 						}
+						}
+						}
+						elseif ($block->getId() == 15 || $block->getId() == 16 || $block->getId() == 21 || $block->getId() == 14 || $block->getId() == 73 || $block->getId() == 56 || $block->getId() == 129 ){
+							$ev->setDrops([Item::get(0, 0, 0)]);
+							$player->sendMessage("§aOres Drops from stone, type /drop for more info");
+							}
 						}}
-						}}}
 						
 						
-						
-						
+
+
+
+public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+	switch($command->getName()){
+		case "drop":
+				if($sender->hasPermission("stonedrop.cmd")) {
+				$sender->sendMessage("§l§7=========§aDrop§7=========");
+				$sender->sendMessage("   §2*  §l§7Coal 2%           ");
+				$sender->sendMessage("   §2*  §l§6Iron Ore 1,8%      ");
+				$sender->sendMessage("   §2*  §l§9Lapiz 1,5%         ");
+				$sender->sendMessage("   §2*  §l§eGold Ore 0,8%      ");
+				$sender->sendMessage("   §2*  §l§4Redstone 1,35%     ");
+				$sender->sendMessage("   §2*  §l§bDiamond Ore 1,2%   ");
+				$sender->sendMessage("   §2*  §l§2Emerald Ore 0,75%  ");
+				$sender->sendMessage("§l§7=======================");
+				}
+				return true;}
+				}}
